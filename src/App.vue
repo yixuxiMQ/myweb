b2b7e06a0ca73013c30678b5fc1c83584385e6c5
Tober <1946433187@qq.com>
2025-12-05 03:23:51 +0800

    将新建组件添加到主页

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import HelloCpt from "./components/HelloCpt.vue";
+import ContentCpt from "./components/ContentCpt.vue";
 </script>
 
 <template>
   <h1>My Web</h1>
   <HelloCpt />
+  <ContentCpt />
 </template>
 
 <style scoped></style>
